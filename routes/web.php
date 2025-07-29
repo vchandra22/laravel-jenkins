06f6059ec079d3a2b849809ff306a514c89c10f6
vchandra22 <vincentchaandra@gmail.com>
2025-07-29 14:11:05 +0700

    feat: check debug scheme rout

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,15 @@ Route::get('dashboard', function () {
 
 Route::get('/debug-scheme', function () {
     return [
+        'sapi' => php_sapi_name(),
+        'is_frankenphp' => isset($_SERVER['FRANKENPHP_VERSION']) ? 'yes' : 'no',
+        'frankenphp_version' => $_SERVER['FRANKENPHP_VERSION'] ?? 'not set',
         'secure' => request()->isSecure(),
         'url' => url('/'),
         'server_https' => $_SERVER['HTTPS'] ?? 'not set',
     ];
 });
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
