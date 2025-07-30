878a8094c773429c628ef4b6fe970893e3150a9d
vchandra22 <vincentchaandra@gmail.com>
2025-07-30 10:43:36 +0700

    feat: a little changes test notify

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -40,7 +40,7 @@ import { Head, Link } from '@inertiajs/vue3';
                 >
                     <h1 class="mb-1 font-medium">Let's get started</h1>
                     <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">
-                        Laravel has an incredibly rich ecosystem. <br />We suggest starting with the following.
+                        Laravel 12 has an incredibly rich ecosystem. <br />We suggest starting with the following.
                     </p>
                     <ul class="mb-4 flex flex-col lg:mb-6">
                         <li
